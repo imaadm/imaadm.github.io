20dffc57e4359cdb78f4587a135ac178e641e61c
Imaad Mohammed <imaadmohammed@gmail.com>
2023-02-13 19:33:34 -0600

    styling setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
-
+import SideNav from "./components/SideNav";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Imaad Mohammed</p>
+        <a href="http://github.com/imaadm">Github</a>
+        <hr className="line"></hr>
       </header>
+      <div className="body">
+        <div className="bodyText">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, 
+            sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <SideNav />
+      </div>
     </div>
   );
 }
---
+++ src/components/SideNav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./sidenav.css";
+function SideNav() {
+  return (
+    <div className="sidenav">
+      <p>About Me</p>
+      <p>Portfolio</p>
+      <p>Links</p>
+    </div>
+  );
+}
+
+export default SideNav;

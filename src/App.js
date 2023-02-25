571c7384a91a467a6b4b2930858c503fe504ca0c
Imaad Mohammed <imaadmohammed@gmail.com>
2023-02-25 17:26:09 -0600

    removed routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import "./App.css";
 import AboutMe from "./components/AboutMe";
-import Portfolio from "./components/Portfolio";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Header from "./components/Header";
 function App() {
   return (
-    <HashRouter>
+    <div>
       <Header />
       <div className="h-[90vh]">
-        <Routes>
-          <Route path="/" element={<AboutMe />} />
-          <Route path="/about" element={<AboutMe />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-        </Routes>
+        <AboutMe />
       </div>
-    </HashRouter>
+    </div>
   );
 }
 
